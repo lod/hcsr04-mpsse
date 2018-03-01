91f758fb617fecadf59cdff83b7d09f5825f4dd0
David Tulloh <git-david@tulloh.id.au>
2018-03-01 13:56:02 +1100

    Clean up and added main loop

--- ftdi_read.c
+++ ftdi_read.c
@@ -2,20 +2,117 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
+#include <signal.h>
 
 #include <libftdi1/ftdi.h>
 
-void print_byte(uint8_t byte) {
-	printf("%c %c %c %c %c %c %c %c\n",
-			byte & 0x80 ? '1' : '0',
-			byte & 0x40 ? '1' : '0',
-			byte & 0x20 ? '1' : '0',
-			byte & 0x10 ? '1' : '0',
-			byte & 0x08 ? '1' : '0',
-			byte & 0x04 ? '1' : '0',
-			byte & 0x02 ? '1' : '0',
-			byte & 0x01 ? '1' : '0'
-	);
+#define LEN 0xFFFF
+
+/* Wiring:
+ *  VCC - VCC (red)
+ *  Trig - GPIOL0 (gray)
+ *  Echo - TDI (green)
+ *  GND - GND (black)
+ */
+
+unsigned int elapsed_ns(const struct timespec *start, const struct timespec *end) {
+	return (end->tv_sec - start->tv_sec)*1e9 + (end->tv_nsec - start->tv_nsec);
+}
+
+void sample(struct ftdi_context *ftdi) {
+	char buf[3];
+	// SET_BITS_LOW configures the first 8 lines <value> <direction>
+	buf[0] = SET_BITS_LOW;
+	buf[1] = 0xFF; // drive outputs high
+	buf[2] = 0x10; // GPIOL0 == output, all others = input
+	int ret;
+	if (ftdi_write_data(ftdi, buf, 3) != 3) {
+		printf("Error: Sending gpio high command\n");
+		return;
+	}
+
+	struct timespec burst = {0,10000}; // 10us
+	nanosleep(&burst, NULL);
+
+	buf[1] = 0; // drive outputs low
+	if (ftdi_write_data(ftdi, buf, 3) != 3) {
+		printf("Error: Sending gpio low command\n");
+		return;
+	}
+
+	buf[0] = 0x2C; // MPSSE_DO_READ | ??
+	buf[1] = (LEN-1) & 0xFF;
+	buf[2] = (LEN-1) >> 8;
+
+	if (ftdi_write_data(ftdi, buf, 3) != 3) {
+		printf("Error: Sending initiate read command\n");
+		return;
+	}
+
+	uint8_t out[LEN] = {0};
+
+	// read doesn't wait, returns up to requested length
+	int sofar = 0;
+	struct timespec ts_start, ts_current;
+	clock_gettime(CLOCK_MONOTONIC, &ts_start);
+	do {
+		ret = ftdi_read_data(ftdi, out+sofar, LEN-sofar);
+		sofar += ret;
+		clock_gettime(CLOCK_MONOTONIC, &ts_current);
+		// printf("rread %d %d %d\n", ret, sofar, elapsed_ns(&ts_start, &ts_current));
+	} while (sofar < LEN && elapsed_ns(&ts_start, &ts_current) < 1e9);
+	if (sofar < LEN) {
+		printf("Error: timeout on read - probably far distance\n");
+		return;
+	}
+
+
+	// Count the number of high bits, this is our pulse length
+	int start = 0, end = LEN;
+	for (int i = 0; i < LEN; i++) {
+		if (out[i] != 0) {
+			start = i*8;
+			uint8_t startb = out[i];
+			while (startb >>= 1) start++;
+			break;
+		}
+	}
+	if (!start) {
+		printf("Error: No high bits\n");
+		return;
+	}
+
+	for (int i = LEN; i; i--) {
+		if (out[i] != 0x00) {
+			end = i*8;
+			uint8_t endb = out[i];
+			while (endb >>= 1) end++;
+			break;
+		}
+	}
+	if (end == LEN) {
+		printf("Error: No pulse end\n");
+		return;
+	}
+
+	printf("Dist %f\n", (float)(end-start)/34800);
+
+	// We default to a 6MHz sample frequency
+	// 425162 samps 6M samps/s = 0.07s
+	// HC-SR04 gives the following formula for air: cm = us/58
+	// 100m = 0.001s/58
+	// samps / 6M = ?s
+	// m = us/5800
+	//   = u(samps / 6M)/5800
+	//   = samps / 34800 (m)
+	//
+	//   TODO: Values seem to be about 90% of real
+}
+
+static int terminate = 0;
+
+void intHandler(int whocares) {
+	terminate = 1;
 }
 
 int main(void) {
@@ -40,105 +137,15 @@ int main(void) {
 
 	printf("type %d\n", ftdi->type); // output to reassure that we are running
 
-	/* Wiring:
-	 *  VCC - VCC (red)
-	 *  	Trig - TDO (yellow)
-	 *  Trig - GPIOL0 (gray)
-	 *  Echo - TDI (green)
-	 *  GND - GND (black)
-	 */
 
-	char buf[3];
-	// SET_BITS_LOW configures the first 8 lines <value> <direction>
-	buf[0] = SET_BITS_LOW;
-	buf[1] = 0xFF; // drive outputs high
-	buf[2] = 0x10; // GPIOL0 == output, all others = input
-	int ret;
-	ret = ftdi_write_data(ftdi, buf, 3);
-	printf("High %d\n", ret);
+	struct sigaction act;
+	act.sa_handler = intHandler;
+	sigaction(SIGINT, &act, NULL);
 
-	struct timespec burst = {0,10000}; // 10us
-	nanosleep(&burst, NULL);
-
-	buf[1] = 0; // drive outputs low
-	ret = ftdi_write_data(ftdi, buf, 3);
-	printf("Low %d\n", ret);
-
-
-	/*
-	uint8_t val;
-	for (int i = 0; i < 100; i++) {
-		ftdi_read_pins(ftdi, &val); print_byte(val);
-	}
-	*/
-
-#define LEN 0xFFFF
-	//buf[0] = MPSSE_DO_READ;
-	buf[0] = 0x2C;
-	buf[1] = (LEN-1) & 0xFF;
-	buf[2] = (LEN-1) >> 8;
-
-	ret = ftdi_write_data(ftdi, buf, 3);
-	printf("read %d\n", ret);
-
-	uint8_t out[LEN] = {0};
-
-	// read doesn't wait, returns up to requested length
-	// TODO: timeout
-	int sofar = 0;
-	do {
-		ret = ftdi_read_data(ftdi, out+sofar, LEN-sofar);
-		sofar += ret;
-		printf("rread %d %d\n", ret, sofar);
-	} while (sofar < LEN);
-
-	for (int i = 0; i < sofar; i++) {
-		print_byte(out[i]);
+	while(!terminate) {
+		sample(ftdi);
 	}
 
-	// Count the number of high bits, this is our pulse length
-	int start = 0, end = LEN;
-	for (int i = 0; i < LEN; i++) {
-		if (out[i] != 0) {
-			start = i*8;
-			uint8_t startb = out[i];
-			while (startb >>= 1) start++;
-			break;
-		}
-	}
-	if (!start) {
-		printf("Error: No high bits\n");
-		goto open_error;
-	}
-
-	for (int i = LEN; i; i--) {
-		if (out[i] != 0x00) {
-			end = i*8;
-			uint8_t endb = out[i];
-			while (endb >>= 1) end++;
-			break;
-		}
-	}
-	if (end == LEN) {
-		printf("Error: No pulse end\n");
-		goto open_error;
-	}
-
-	printf("PULSE %d-%d = %d\n", start, end, end-start);
-	printf("Dist %f", (float)(end-start)/34800);
-
-	// We default to a 6MHz sample frequency
-	// 425162 samps 6M samps/s = 0.07s
-	// HC-SR04 gives the following formula for air: cm = us/58
-	// 100m = 0.001s/58
-	// samps / 6M = ?s
-	// m = us/5800
-	//   = u(samps / 6M)/5800
-	//   = samps / 34800 (m)
-
-
-	//printf("Pausing\n");
-	//getchar();
 
 
 	ftdi_usb_close(ftdi);
